ad04e59ec6822485c8975062074c0c5d017acb72
petr.sturc <petr.sturc@gmail.com>
2012-10-20 21:13:16 +0200

    Firefox option added to run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,11 @@
 start erl -sname sail_web -pa ebin -pa debs/erlang_template/ebin -pa deps/cowboy/ebin -pa deps/jsx/ebin -pa apps/sail_server/ebin -pa apps/sail_web/ebin -s sail_web_app -s reloader
 sleep 5
+if "%1"=="-f" goto FIREFOX
 "D:\Documents and Settings\psturc\Local Settings\Application Data\Google\Chrome\Application\chrome.exe" http://localhost:8080/static/index.html
+GOTO DONE
+
+
+:FIREFOX
+"c:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8080/static/index.html
+
+:DONE
